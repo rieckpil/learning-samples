477fa030b052a58289d4b3f500a0e06bab8b465a
Philip Riecks <philip@riecks.io>
2018-04-03 17:54:10 +0200

    add project for effective java

---
+++ effective-java/src/main/java/de/rieckpil/learning/EffectiveJavaApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveJavaApplication.class, args);
+	}
+}
