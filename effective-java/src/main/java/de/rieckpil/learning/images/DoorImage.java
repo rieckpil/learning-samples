dad923cf1890873402c2c890c75e08ab209c246b
Philip Riecks <philip@riecks.io>
2018-04-06 16:54:08 +0200

    add new image challenge

---
+++ effective-java/src/main/java/de/rieckpil/learning/images/DoorImage.java
@@ -0,0 +1,30 @@
+package de.rieckpil.learning.images;
+
+public class DoorImage {
+	
+	public static void main(String[] args) {
+
+		/** Create following door image with nested for-loops:
+		 * 
+		 * ############
+		 * ##        ##
+		 * # #      # #
+		 * #  #    #  #
+		 * #   #  #   #   
+		 * #    ##    #
+		 * #   #  #   #
+		 * #  #    #  #
+		 * # #      # #
+		 * ##        ## 
+		 * ############
+		 * 
+		 */
+		
+		draw();
+	}
+	
+	private static void draw() {
+		
+	}
+
+}
