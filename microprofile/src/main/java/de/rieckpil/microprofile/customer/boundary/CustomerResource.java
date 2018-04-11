2614c33a155afc3223f8cc18477277e936e174b6
Philip Riecks <philip@riecks.io>
2018-04-11 06:26:29 +0200

    add new jax rs endpoint with a path variable

--- microprofile/src/main/java/de/rieckpil/microprofile/customer/boundary/CustomerResource.java
+++ microprofile/src/main/java/de/rieckpil/microprofile/customer/boundary/CustomerResource.java
@@ -2,12 +2,19 @@ package de.rieckpil.microprofile.customer.boundary;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/customer")
 public class CustomerResource {
-	
+
 	@GET
 	public String sayHello() {
 		return "Hello World";
+	}
+
+	@GET
+	@Path("/{id}")
+	public String getSpecificHelloWorld(@PathParam("id") String id) {
+		return "Hello World " + id;
 	}
 }
