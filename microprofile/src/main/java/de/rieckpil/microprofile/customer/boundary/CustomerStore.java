9fd961288e6bd1b05aa05f593d5a3eac66cf9653
Philip Riecks <philip@riecks.io>
2018-04-23 23:12:53 +0200

    use config in microprofile service

--- microprofile/src/main/java/de/rieckpil/microprofile/customer/boundary/CustomerStore.java
+++ microprofile/src/main/java/de/rieckpil/microprofile/customer/boundary/CustomerStore.java
@@ -2,11 +2,14 @@ package de.rieckpil.microprofile.customer.boundary;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import de.rieckpil.microprofile.customer.entity.Adress;
 import de.rieckpil.microprofile.customer.entity.Customer;
@@ -16,19 +19,21 @@ public class CustomerStore {
 
 	private List<Customer> customers;
 
+	@Inject
+	@ConfigProperty(name = "amount.customers", defaultValue = "20")
+	private int amountOfCustomers;
+
 	@PostConstruct
 	public void init() {
-		
-		customers = new ArrayList<Customer>();
-		
-		Customer max = new Customer("Max", "Mustermann", LocalDateTime.of(1995, 9, 13, 0, 0),
-				new Adress(91074, "Hauptstraße 14", "Herzogenaurach"));
-		Customer tom = new Customer("Tom", "Meier", LocalDateTime.of(1990, 9, 13, 0, 0),
-				new Adress(91084, "Hauptstraße 14", "Erlangen"));
-		Customer frank = new Customer("Frank", "Schuster", LocalDateTime.of(1996, 9, 15, 0, 0),
-				new Adress(91084, "Am Buck 13", "Erlangen"));
 
-		customers.addAll(Arrays.asList(max, tom, frank));
+		customers = new ArrayList<Customer>();
+
+		for (int i = 0; i < amountOfCustomers; i++) {
+			Customer customer = new Customer("Max", "Mustermann",
+					LocalDateTime.of(ThreadLocalRandom.current().nextInt(1950, 2005), 9, 13, 0, 0),
+					new Adress(ThreadLocalRandom.current().nextInt(4000, 9800), "Hauptstraße 14", "Herzogenaurach"));
+			customers.add(customer);
+		}
 
 	}
 
