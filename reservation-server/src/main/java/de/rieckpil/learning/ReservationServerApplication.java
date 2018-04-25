4bd5f50f7b01b7bc2cd93218fcb178e7cda5de77
Philip Riecks <philip@riecks.io>
2018-04-25 22:42:10 +0200

    add missing files for reservations server spring boot app

---
+++ reservation-server/src/main/java/de/rieckpil/learning/ReservationServerApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServerApplication.class, args);
+    }
+}
