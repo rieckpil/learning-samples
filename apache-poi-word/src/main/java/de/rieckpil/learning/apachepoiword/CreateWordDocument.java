3a1c26145ab76c19b5e26d279dcc4d821570fda3
Philip Riecks <philip@riecks.io>
2018-05-01 16:00:20 +0200

    add example for creating word document with apache poi

---
+++ apache-poi-word/src/main/java/de/rieckpil/learning/apachepoiword/CreateWordDocument.java
@@ -0,0 +1,43 @@
+package de.rieckpil.learning.apachepoiword;
+
+import org.apache.poi.xwpf.usermodel.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+@Component
+public class CreateWordDocument implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        XWPFDocument document = new XWPFDocument();
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("Build Your REST API with Spring");
+        titleRun.setColor("009933");
+        titleRun.setBold(true);
+        titleRun.setFontFamily("Courier");
+        titleRun.setFontSize(20);
+
+        XWPFParagraph subTitle = document.createParagraph();
+        subTitle.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun subTitleRun = subTitle.createRun();
+        subTitleRun.setText("from HTTP fundamentals to API Mastery");
+        subTitleRun.setColor("00CC44");
+        subTitleRun.setFontFamily("Courier");
+        subTitleRun.setFontSize(16);
+        subTitleRun.setTextPosition(20);
+        subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
+
+        FileOutputStream out = new FileOutputStream(new File("/Users/Philip/Desktop/HelloWorld.docx"));
+        document.write(out);
+        out.close();
+        document.close();
+
+    }
+}
---
+++ apache-poi-word/src/test/java/de/rieckpil/learning/apachepoiword/ApachePoiWordApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.apachepoiword;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApachePoiWordApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
