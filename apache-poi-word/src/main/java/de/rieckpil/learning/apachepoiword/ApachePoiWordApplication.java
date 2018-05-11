ed9986c890ca5874cd8a99ea55a4932f41425d90
Philip Riecks <philip@riecks.io>
2018-05-11 16:06:59 +0200

    add performance test for creating about 500 reports with xdocreport

--- apache-poi-word/src/main/java/de/rieckpil/learning/apachepoiword/ApachePoiWordApplication.java
+++ apache-poi-word/src/main/java/de/rieckpil/learning/apachepoiword/ApachePoiWordApplication.java
@@ -1,12 +1,23 @@
 package de.rieckpil.learning.apachepoiword;
 
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApachePoiWordApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(ApachePoiWordApplication.class, args);
+
+        XDocReportExample example = new XDocReportExample();
+
+        for (int i = 0; i < 499; i++) {
+            example.createPdfFromTemplateWithXDocReport("/templates/Invoice.docx",
+                    "/Users/Philip/Desktop/junk/pdf/invoice_out" + i + ".pdf");
+        }
+
+        // framework is caching the reports -> XDocReportRegistry.getRegistry().getCachedReports().clear();
+
     }
 }
