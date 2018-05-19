a075ce9a96d3be135aa7057b0fbaea0a3a8440cf
Philip Riecks <philip@riecks.io>
2018-05-19 18:20:12 +0200

    create new spring project for spring boot 2 book

---
+++ spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/SpringBoot2BookApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springboot2book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2BookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2BookApplication.class, args);
+    }
+}
---
+++ spring-boot-2-book/src/test/java/de/rieckpil/learning/springboot2book/SpringBoot2BookApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.springboot2book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2BookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
