2935ac96bfd210c3bed549b42b9b1186770d72be
Philip Riecks <philip@riecks.io>
2018-05-20 16:23:11 +0200

    customize banner with ASCII art and add color and properties

--- spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/HelloWorldController.java
+++ spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/HelloWorldController.java
@@ -1,5 +1,7 @@
 package de.rieckpil.learning.springboot2book;
 
+import org.hibernate.validator.internal.constraintvalidators.bv.number.bound.MinValidatorForLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
