af7cfa4d101e93b130b6a867941cc8909497572c
Philip Riecks <philip@riecks.io>
2018-05-24 06:43:50 +0200

    add sample image returning controller method with etags and cache control

--- spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/HelloWorldController.java
+++ spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/HelloWorldController.java
@@ -5,11 +5,19 @@ import org.hibernate.validator.internal.constraintvalidators.bv.number.bound.Min
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Locale;
+
+import static org.springframework.util.DigestUtils.md5DigestAsHex;
 
 @RestController
 public class HelloWorldController {
@@ -38,6 +46,27 @@ public class HelloWorldController {
 
         Person p1 = new Person(1, "Philip", 22);
         return p1;
+
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<Resource> getImage() throws IOException {
+
+        final Resource image;
+        final String etag;
+
+        CacheControl cacheControl = CacheControl.noCache();
+
+        image = new ClassPathResource("/image.png");
+        etag = md5DigestAsHex(image.getInputStream());
+
+        return ResponseEntity
+                    .ok()
+                    .cacheControl(cacheControl)
+                    .lastModified(image.lastModified())
+                    .eTag(etag)
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(image);
 
     }
 
