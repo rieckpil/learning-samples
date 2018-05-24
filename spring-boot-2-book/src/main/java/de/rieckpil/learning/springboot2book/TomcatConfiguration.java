028f3274de0f3b20f48250a41da9ac8cc903abbe
Philip Riecks <philip@riecks.io>
2018-05-24 06:09:19 +0200

    add example for configuring tomcat servlet container

---
+++ spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/TomcatConfiguration.java
@@ -0,0 +1,15 @@
+package de.rieckpil.learning.springboot2book;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class TomcatConfiguration implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+       factory.addConnectorCustomizers(connector -> {
+           connector.setProxyName("mycorporateproxy");
+           connector.setProxyPort(8080);
+       });
+    }
+}
