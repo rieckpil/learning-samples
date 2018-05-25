e555c143d014e0b33ed2154af90083892a54e2e1
Philip Riecks <philip@riecks.io>
2018-05-25 20:58:16 +0200

    add new project for spring data chapter

---
+++ spring-boot-2-book-data/src/main/java/de/rieckpil/learning/springboot2bookdata/SpringBoot2BookDataApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springboot2bookdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2BookDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2BookDataApplication.class, args);
+    }
+}
---
+++ spring-boot-2-book-data/src/test/java/de/rieckpil/learning/springboot2bookdata/SpringBoot2BookDataApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.springboot2bookdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2BookDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
