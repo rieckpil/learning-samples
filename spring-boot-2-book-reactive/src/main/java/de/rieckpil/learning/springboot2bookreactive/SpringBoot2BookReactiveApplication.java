9c78e2a79775771471f54ec2351ddd12994989c8
Philip Riecks <philip@riecks.io>
2018-05-27 17:13:56 +0200

    add reactive project

---
+++ spring-boot-2-book-reactive/src/main/java/de/rieckpil/learning/springboot2bookreactive/SpringBoot2BookReactiveApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springboot2bookreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2BookReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2BookReactiveApplication.class, args);
+    }
+}
