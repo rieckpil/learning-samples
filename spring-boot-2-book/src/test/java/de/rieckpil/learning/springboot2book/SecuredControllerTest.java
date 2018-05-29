b303e24c6845f74abbca6a1911c2a6efde72a067
Philip Riecks <philip@riecks.io>
2018-05-29 19:01:58 +0200

    add WebMvcTest for SecuredController

--- spring-boot-2-book/src/test/java/de/rieckpil/learning/springboot2book/SecuredControllerTest.java
+++ spring-boot-2-book/src/test/java/de/rieckpil/learning/springboot2book/SecuredControllerTest.java
@@ -2,25 +2,37 @@ package de.rieckpil.learning.springboot2book;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.security.Principal;
+
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(SecuredController.class)
 public class SecuredControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private SecuredService securedService;
+
     @Test
     public void getGreetingShouldWork() throws Exception{
+
+        when(securedService.greetPrincipal(any(Principal.class))).thenReturn("Hello, Tester.");
+
         mockMvc.perform(get("/greeting").with(user("Tester"))).andExpect(content().string("Hello, Tester."));
     }
 
