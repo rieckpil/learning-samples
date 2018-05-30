dfcef2faf90f4bc838edd0469ee141d4af582f24
Philip Riecks <philip@riecks.io>
2018-05-30 18:30:07 +0200

    add security filter for specific actuator endpoints

--- spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/configuration/CustomHttpSecurityConfig.java
+++ spring-boot-2-book/src/main/java/de/rieckpil/learning/springboot2book/configuration/CustomHttpSecurityConfig.java
@@ -1,5 +1,8 @@
 package de.rieckpil.learning.springboot2book.configuration;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +20,12 @@ public class CustomHttpSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .headers()
                     .frameOptions().disable()
                     .cacheControl().disable()
+                .and()
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.to(MetricsEndpoint.class, HealthEndpoint.class))
+                    .permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint())
+                    .authenticated()
                 .and()
                 .antMatcher("/**")
                 .authorizeRequests()
