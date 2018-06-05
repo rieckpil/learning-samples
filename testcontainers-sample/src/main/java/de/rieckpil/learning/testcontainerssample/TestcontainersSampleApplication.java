4c408cba06f484048d4417acbd1779f31dc4d9ec
Philip Riecks <philip@riecks.io>
2018-06-05 06:17:48 +0200

    add missing files for testcontainers example

---
+++ testcontainers-sample/src/main/java/de/rieckpil/learning/testcontainerssample/TestcontainersSampleApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.testcontainerssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcontainersSampleApplication.class, args);
+    }
+}
