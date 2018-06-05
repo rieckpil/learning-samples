797bbd21a0b615cc0da1591cfa1104dbc8aa3c8a
Philip Riecks <philip@riecks.io>
2018-06-05 06:41:51 +0200

    add find by id method to person boundary

---
+++ testcontainers-sample/src/main/java/de/rieckpil/learning/testcontainerssample/NoPersonFoundException.java
@@ -0,0 +1,8 @@
+package de.rieckpil.learning.testcontainerssample;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NoPersonFoundException extends RuntimeException {
+}
--- testcontainers-sample/src/main/java/de/rieckpil/learning/testcontainerssample/PersonBoundary.java
+++ testcontainers-sample/src/main/java/de/rieckpil/learning/testcontainerssample/PersonBoundary.java
@@ -3,6 +3,7 @@ package de.rieckpil.learning.testcontainerssample;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,11 @@ public class PersonBoundary {
     @GetMapping("/persons")
     public ResponseEntity<List<Person>> getAllPersons() {
         return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/persons/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(personRepository.findById(id).orElseThrow(() -> new NoPersonFoundException
+                ()), HttpStatus.OK);
     }
  }
