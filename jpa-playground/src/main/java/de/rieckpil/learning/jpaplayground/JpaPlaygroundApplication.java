89daada3bd4810e26db3110150fb1fd0e357deda
Philip Riecks <philip@riecks.io>
2018-06-11 06:23:31 +0200

    add missing files for jpa-playground project

---
+++ jpa-playground/src/main/java/de/rieckpil/learning/jpaplayground/JpaPlaygroundApplication.java
@@ -0,0 +1,17 @@
+package de.rieckpil.learning.jpaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+public class JpaPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPlaygroundApplication.class, args);
+    }
+}
