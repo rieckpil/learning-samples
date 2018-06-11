0035ed89d070b1cb09d2f4e539def59532549361
Philip Riecks <philip@riecks.io>
2018-06-11 06:22:36 +0200

    init new project for high performance jpa book

---
+++ high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/HighPerformanceJpaApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.highperformancejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPerformanceJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighPerformanceJpaApplication.class, args);
+    }
+}
