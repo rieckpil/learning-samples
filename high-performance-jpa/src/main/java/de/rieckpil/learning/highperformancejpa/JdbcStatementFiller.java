09ce2f3324687f6f616d57401d820e1b65d35acc
Philip Riecks <rieckpil@schaeffler.com>
2018-06-12 13:26:20 +0200

    add example for limiting result set size

--- high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/JdbcStatementFiller.java
+++ high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/JdbcStatementFiller.java
@@ -1,6 +1,9 @@
 package de.rieckpil.learning.highperformancejpa;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,15 +19,41 @@ public class JdbcStatementFiller implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
 
-    boolean isFirstResultAResultSet = jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('Hans')");
-    System.out.println("isFirstResultAResultSet = " + isFirstResultAResultSet);
+    runPreparedStatement();
+    insertMultiplePersons();
+
+    PreparedStatement preparedSelect = jdbcTemplate.getDataSource().getConnection().prepareStatement("SELECT * FROM person");
+    preparedSelect.setFetchSize(5);
+    preparedSelect.setMaxRows(10);
+
+    ResultSet executeQuery = preparedSelect.executeQuery();
+
+    System.out.println("Amount of columns: " + executeQuery.getMetaData().getColumnCount());
+
+    while (executeQuery.next()) {
+      System.out.println(executeQuery.getString("name"));
+    }
+
+  }
+
+  private void runPreparedStatement() throws SQLException {
 
     PreparedStatement preparedStatement = jdbcTemplate.getDataSource().getConnection().prepareStatement("INSERT INTO person (name) VALUES (?)");
     preparedStatement.setString(1, "Paul");
 
-    isFirstResultAResultSet = preparedStatement.execute();
+    boolean isFirstResultAResultSet = preparedStatement.execute();
 
     System.out.println("isFirstResultAResultSet = " + isFirstResultAResultSet);
+  }
+
+  private void insertMultiplePersons() throws Exception {
+
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
+    jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('" + ThreadLocalRandom.current().nextInt() + "')");
 
   }
 }
