8ef5bef44f4d94c6766fdd180bf6850dc0054ddf
Philip Riecks <rieckpil@schaeffler.com>
2018-06-12 13:01:07 +0200

    add example for plain jdbc statements

--- high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/JdbcStatementFiller.java
+++ high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/JdbcStatementFiller.java
@@ -1,28 +1,30 @@
 package de.rieckpil.learning.highperformancejpa;
 
+import java.sql.PreparedStatement;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.PreparedStatement;
-
 @Component
 public class JdbcStatementFiller implements CommandLineRunner {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-    @Override
-    public void run(String... args) throws Exception {
+  @Override
+  public void run(String... args) throws Exception {
 
+    boolean isFirstResultAResultSet = jdbcTemplate.getDataSource().getConnection().createStatement().execute("INSERT INTO person (name) VALUES ('Hans')");
+    System.out.println("isFirstResultAResultSet = " + isFirstResultAResultSet);
 
-        PreparedStatement preparedStatement = jdbcTemplate.getDataSource().getConnection().prepareStatement("INSERT INTO person (name) VALUES (?)");
-        preparedStatement.setString(1, "Paul");
-        
-        boolean isFirstResultAResultSet = preparedStatement.execute();
+    PreparedStatement preparedStatement = jdbcTemplate.getDataSource().getConnection().prepareStatement("INSERT INTO person (name) VALUES (?)");
+    preparedStatement.setString(1, "Paul");
 
-        System.out.println("isFirstResultAResultSet = " + isFirstResultAResultSet);
+    isFirstResultAResultSet = preparedStatement.execute();
 
-    }
+    System.out.println("isFirstResultAResultSet = " + isFirstResultAResultSet);
+
+  }
 }
