54759e96cf2c85d623c938b6071bda61b624f159
Philip Riecks <philip@riecks.io>
2018-06-24 19:10:25 +0200

    add new project for jooq with spring boot

---
+++ spring-boot-jooq-example/src/main/java/de/rieckpil/learning/springbootjooqexample/SpringBootJooqExampleApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springbootjooqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJooqExampleApplication.class, args);
+    }
+}
---
+++ spring-boot-jooq-example/src/test/java/de/rieckpil/learning/springbootjooqexample/SpringBootJooqExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.springbootjooqexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJooqExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
