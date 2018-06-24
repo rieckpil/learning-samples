8d1626894e62f84cd7c93dee8b676c8e954e17f3
Philip Riecks <philip@riecks.io>
2018-06-24 15:50:02 +0200

    add example for query caching

---
+++ high-performance-jpa/src/main/java/de/rieckpil/learning/highperformancejpa/HibernateQueryCaching.java
@@ -0,0 +1,38 @@
+package de.rieckpil.learning.highperformancejpa;
+
+import de.rieckpil.learning.highperformancejpa.entity.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.cache.internal.StandardQueryCache;
+import org.hibernate.jpa.QueryHints;
+import org.hibernate.stat.SecondLevelCacheStatistics;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Slf4j
+@Component
+@Transactional
+public class HibernateQueryCaching implements CommandLineRunner {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void run(String... args) {
+
+        List<Person> persons = entityManager.createQuery("SELECT p FROM Person p ORDER BY p.id", Person.class)
+                .setMaxResults(3)
+                .setHint(QueryHints.HINT_CACHEABLE, true)
+                .getResultList();
+
+        for (Person p : persons) {
+            System.out.println("p = " + p);
+        }
+
+    }
+}
