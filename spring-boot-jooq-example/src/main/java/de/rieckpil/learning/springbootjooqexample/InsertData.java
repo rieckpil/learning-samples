3e3bdea7553b35e1ef540bda1944764064bc59be
Philip Riecks <philip@riecks.io>
2018-06-24 19:45:45 +0200

    add simple upsert with jOOQ

--- spring-boot-jooq-example/src/main/java/de/rieckpil/learning/springbootjooqexample/InsertData.java
+++ spring-boot-jooq-example/src/main/java/de/rieckpil/learning/springbootjooqexample/InsertData.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
-import static org.jooq.impl.DSL.field;
-import static org.jooq.impl.DSL.table;
 import static de.rieckpil.learning.springbootjooqexample.domain.tables.Posts.POSTS;
 
 @Component
@@ -23,8 +21,23 @@ public class InsertData implements CommandLineRunner {
         // @formatter:off
         dslContext
                 .insertInto(POSTS)
-                .columns(POSTS.TITLE, POSTS.CONTENT, POSTS.CREATED_ON)
-                .values("Hello World", "Lorem ipsum dolor sit amet", Instant.now().getEpochSecond())
+                .columns(POSTS.ID, POSTS.TITLE, POSTS.CONTENT, POSTS.CREATED_ON)
+                .values(1L, "Hello World", "Lorem ipsum dolor sit amet", Instant.now().getEpochSecond())
+                .execute();
+
+        dslContext
+                .insertInto(POSTS)
+                .columns(POSTS.ID, POSTS.TITLE, POSTS.CONTENT, POSTS.CREATED_ON)
+                .values(2L, "Hello World2", "Lorem ipsum dolor sit amet2", Instant.now().getEpochSecond())
+                .execute();
+
+        dslContext
+                .insertInto(POSTS)
+                .columns(POSTS.ID, POSTS.TITLE, POSTS.CONTENT, POSTS.CREATED_ON)
+                .values(1L, "Upsert", "Upserts are cool!", Instant.now().getEpochSecond())
+                .onDuplicateKeyUpdate()
+                .set(POSTS.TITLE, "Upsert")
+                .set(POSTS.CONTENT, "Upserts are cool!")
                 .execute();
         // @formatter:on
 
