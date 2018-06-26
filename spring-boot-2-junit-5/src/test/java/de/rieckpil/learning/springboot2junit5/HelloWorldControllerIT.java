639931bef009bb92289775109bafe71bd8c6f48d
Philip Riecks <rieckpil@schaeffler.com>
2018-06-26 13:30:56 +0200

    add simple @SpringBootTest with JUnit 5

---
+++ spring-boot-2-junit-5/src/test/java/de/rieckpil/learning/springboot2junit5/HelloWorldControllerIT.java
@@ -0,0 +1,43 @@
+package de.rieckpil.learning.springboot2junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+public class HelloWorldControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetAllPersons() {
+
+        ResponseEntity<Person[]> personResult = restTemplate.getForEntity("http://localhost:" + port + "/persons", Person[].class);
+
+        assertNotNull(personResult.getBody());
+        assertEquals(HttpStatus.OK, personResult.getStatusCode());
+        assertEquals(2, personResult.getBody().length);
+        assertEquals("Philip", personResult.getBody()[0].getFirstName());
+        assertEquals("Riecks", personResult.getBody()[0].getLastName());
+        assertEquals(LocalDate.of(1995, 9, 13).toEpochDay(), personResult.getBody()[0].getDob().toEpochDay());
+
+    }
+}
