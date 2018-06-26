40896322eaa2197d7057467d0e16413a65900287
Philip Riecks <philip@riecks.io>
2018-06-26 07:02:47 +0200

    add new project for spring boot 2 with Junit 5

---
+++ spring-boot-2-junit-5/src/main/java/de/rieckpil/learning/springboot2junit5/SpringBoot2Junit5Application.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springboot2junit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Junit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Junit5Application.class, args);
+    }
+}
---
+++ spring-boot-2-junit-5/src/test/java/de/rieckpil/learning/springboot2junit5/SpringBoot2Junit5ApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.springboot2junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringBoot2Junit5ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
