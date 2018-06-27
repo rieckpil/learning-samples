15cf9aba92a7a9540020482a4199fe252245cacd
Philip Riecks <philip@riecks.io>
2018-06-27 06:48:13 +0200

    add empty project for jsf book

--- react-up-and-running/src/App.js
+++ react-up-and-running/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    componentWillUpdate = () => {
+        console.log('Component will update');
+    }
+
+    componentWillMount = () => {
+        console.log('Component will mount');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React with rieckpil!</h1>
+                </header>
+                <p className="App-intro">
+                    <textarea></textarea>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
