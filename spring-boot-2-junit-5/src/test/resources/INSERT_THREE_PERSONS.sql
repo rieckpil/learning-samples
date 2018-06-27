7b1ae115399f84ea76699777b5881fbe86d7b6a4
Philip Riecks <rieckpil@schaeffler.com>
2018-06-27 08:55:19 +0200

    add simple insert script for testing purposes

---
+++ spring-boot-2-junit-5/src/test/resources/INSERT_THREE_PERSONS.sql
@@ -0,0 +1,3 @@
+INSERT INTO person (id, dob, first_name, last_name) VALUES (1001, '1990-12-24', 'Mike', 'Deere');
+INSERT INTO person (id, dob, first_name, last_name) VALUES (1002, '1999-01-12', 'Peter', 'Parker');
+INSERT INTO person (id, dob, first_name, last_name) VALUES (1003, '1990-06-15', 'Bat', 'Man');
