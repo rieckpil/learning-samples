62cd5e410b23d41894f02204328bfb04bef7de6c
Philip Riecks <philip@riecks.io>
2018-06-28 06:57:51 +0200

    add internal iterator

--- streams-and-lambdas/src/main/java/de/rieckpil/learning/streamsandlambdas/LambdaStart.java
+++ streams-and-lambdas/src/main/java/de/rieckpil/learning/streamsandlambdas/LambdaStart.java
@@ -1,14 +1,22 @@
 package de.rieckpil.learning.streamsandlambdas;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LambdaStart {
 
     public static void main(String[] args) {
 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers.forEach(integer -> System.out.println(integer));
+
+    }
+
+
+    public void lambdaForInnerAnonymousClasses() {
         Thread th = new Thread(() -> System.out.println("in thread"));
-
         th.start();
-
         System.out.println("In main");
-
     }
 }
