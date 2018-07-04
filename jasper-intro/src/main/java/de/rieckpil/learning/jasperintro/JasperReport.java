958e60cb3973ffc92438a88af92a2c9e3a7b1139
Philip Riecks <philip@riecks.io>
2018-07-04 08:32:44 +0200

    add jasper dependencies

---
+++ jasper-intro/src/main/java/de/rieckpil/learning/jasperintro/JasperReport.java
@@ -0,0 +1,13 @@
+package de.rieckpil.learning.jasperintro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JasperReport {
+
+    @GetMapping("/jasper")
+    public void getJasperReport() {
+
+    }
+}
