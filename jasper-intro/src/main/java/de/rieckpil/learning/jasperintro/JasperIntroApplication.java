8d0939f789b7b118942ec1abf17d567924e19cc3
Philip Riecks <philip@riecks.io>
2018-07-08 12:11:07 +0200

    add simple performance test for generating 500 jasper pdf reports

--- jasper-intro/src/main/java/de/rieckpil/learning/jasperintro/JasperIntroApplication.java
+++ jasper-intro/src/main/java/de/rieckpil/learning/jasperintro/JasperIntroApplication.java
@@ -1,10 +1,36 @@
 package de.rieckpil.learning.jasperintro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
 public class JasperIntroApplication {
+
+    @Autowired
+    private PerformanceTester performanceTester;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+
+            String pdfOutputFileName = "/Users/Philip/Desktop/junk/jasper/performance/report";
+
+            long start = System.currentTimeMillis();
+
+            for (int i = 0; i < 500; i++) {
+                performanceTester.createJasperReport(pdfOutputFileName + i + ".pdf",
+                        100);
+            }
+
+            System.out.println("took: " + ((System.currentTimeMillis() - start) / 1000) + " seconds");
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JasperIntroApplication.class, args);
---
+++ jasper-intro/src/main/java/de/rieckpil/learning/jasperintro/PerformanceTester.java
@@ -0,0 +1,114 @@
+package de.rieckpil.learning.jasperintro;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class PerformanceTester {
+
+    private String logoPath;
+    private JasperReport jasperReport;
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        this.logoPath = ClassLoader.getSystemResource("jasper/spring-boot-logo.png").getPath();
+
+        InputStream inputStreamJasperReport = this.getClass().getClassLoader().getResourceAsStream
+                ("jasper/performance.jrxml");
+
+        JasperDesign jasperDesign = JRXmlLoader.load(inputStreamJasperReport);
+        this.jasperReport = JasperCompileManager.compileReport(jasperDesign);
+
+    }
+
+    public void createJasperReport(String outputPdfFileName, int amountOfBeans) {
+
+        JRBeanCollectionDataSource beanColDataSource =
+                new JRBeanCollectionDataSource(createPersons(amountOfBeans));
+
+        Map parameters = new HashMap();
+        parameters.put("ReportTitle", "Performance Test: " + outputPdfFileName);
+        parameters.put("Author", "Prepared By Rieckpil");
+        parameters.put("logo", this.logoPath);
+
+        try {
+            JasperPrint jprint = JasperFillManager.fillReport(
+                    this.jasperReport, parameters, beanColDataSource);
+
+            JasperExportManager.exportReportToPdfFile(jprint, outputPdfFileName);
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private List<Person> createPersons(int amountOfPersons) {
+
+        return Stream.iterate(0, n -> n + 1)
+                .map(e -> new Person("Philip", "Riecks", UUID.randomUUID().toString(), ThreadLocalRandom.current()
+                        .nextLong()))
+                .limit(amountOfPersons)
+                .collect(Collectors.toList());
+    }
+
+    public class Person {
+
+        private String firstName;
+        private String lastName;
+        private String id;
+        private long number;
+
+        public Person(String firstName, String lastName, String id, long number) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.number = number;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public long getNumber() {
+            return number;
+        }
+
+        public void setNumber(long number) {
+            this.number = number;
+        }
+    }
+}
