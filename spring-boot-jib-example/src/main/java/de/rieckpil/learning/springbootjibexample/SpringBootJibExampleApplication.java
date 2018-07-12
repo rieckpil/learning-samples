8d08d3e2a86e60649fbcb3367df16a02c478584b
Philip Riecks <rieckpil@schaeffler.com>
2018-07-12 12:20:52 +0200

    add sample project for evaluating Googles Jib

---
+++ spring-boot-jib-example/src/main/java/de/rieckpil/learning/springbootjibexample/SpringBootJibExampleApplication.java
@@ -0,0 +1,11 @@
+package de.rieckpil.learning.springbootjibexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SpringBootJibExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJibExampleApplication.class, args);
+    }
+}
---
+++ spring-boot-jib-example/src/test/java/de/rieckpil/learning/springbootjibexample/SpringBootJibExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rieckpil.learning.springbootjibexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class SpringBootJibExampleApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
