9ab5d72021032f33490fe36b4eab245a4c8122e6
Philip Riecks <philip@riecks.io>
2018-07-17 07:14:23 +0200

    add sample project for java by comparison book

---
+++ java-by-comparison/src/main/java/de/rieckpil/learning/javabycomparison/JavaByComparisonApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.javabycomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaByComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaByComparisonApplication.class, args);
+    }
+}
