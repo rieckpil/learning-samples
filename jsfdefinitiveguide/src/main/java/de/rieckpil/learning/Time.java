f4246fa99f16174f7edaa8664a99aee88deb06bd
Philip Riecks <philip@riecks.io>
2018-07-20 15:51:07 +0200

    add datetime example

---
+++ jsfdefinitiveguide/src/main/java/de/rieckpil/learning/Time.java
@@ -0,0 +1,105 @@
+package de.rieckpil.learning;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.time.*;
+import java.util.Date;
+
+@Named
+@RequestScoped
+public class Time {
+
+    private Date date;
+    private Date time;
+    private Date both;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private LocalDateTime localDateTime;
+    private OffsetTime offsetTime;
+    private OffsetDateTime offsetDateTime;
+    private ZonedDateTime zonedDateTime;
+
+    public void submit() {
+        System.out.println("date: " + date);
+        System.out.println("time: " + time);
+        System.out.println("both: " + both);
+        System.out.println("localDate: " + localDate);
+        System.out.println("localTime: " + localTime);
+        System.out.println("localDateTime: " + localDateTime);
+        System.out.println("offsetTime: " + offsetTime);
+        System.out.println("offsetDateTime: " + offsetDateTime);
+        System.out.println("zonedDateTime: " + zonedDateTime);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Date getBoth() {
+        return both;
+    }
+
+    public void setBoth(Date both) {
+        this.both = both;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public OffsetTime getOffsetTime() {
+        return offsetTime;
+    }
+
+    public void setOffsetTime(OffsetTime offsetTime) {
+        this.offsetTime = offsetTime;
+    }
+
+    public OffsetDateTime getOffsetDateTime() {
+        return offsetDateTime;
+    }
+
+    public void setOffsetDateTime(OffsetDateTime offsetDateTime) {
+        this.offsetDateTime = offsetDateTime;
+    }
+
+    public ZonedDateTime getZonedDateTime() {
+        return zonedDateTime;
+    }
+
+    public void setZonedDateTime(ZonedDateTime zonedDateTime) {
+        this.zonedDateTime = zonedDateTime;
+    }
+}
