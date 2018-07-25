1096edddcba4cf06ce7edb375dc9b16d8a2a56fa
Philip Riecks <philip@riecks.io>
2018-07-25 22:09:46 +0200

    udpate websocket test with JSF

--- jsfdefinitiveguide/src/main/java/de/rieckpil/learning/Chat.java
+++ jsfdefinitiveguide/src/main/java/de/rieckpil/learning/Chat.java
@@ -1,10 +1,21 @@
 package de.rieckpil.learning;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
+import javax.faces.event.WebsocketEvent;
 import javax.faces.push.Push;
 import javax.faces.push.PushContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Named
 @RequestScoped
@@ -15,12 +26,50 @@ public class Chat {
     private PushContext test;
 
     @Inject
-    @Push(channel = "chat")
+    @Push(channel = "chat2")
     private PushContext chat;
+
+    private Map<Serializable, AtomicInteger> users;
+
+    @PostConstruct
+    public void init() {
+        users = new ConcurrentHashMap<>();
+    }
+
+    public void onopen(@Observes @WebsocketEvent.Opened WebsocketEvent event) {
+        if ("chat".equals(event.getChannel())) {
+            getCounter(event.getUser()).incrementAndGet();
+        }
+    }
+
+    public void onclose(@Observes @WebsocketEvent.Closed WebsocketEvent event) {
+        if ("chat".equals(event.getChannel())) {
+            getCounter(event.getUser()).decrementAndGet();
+        }
+    }
+
+    private AtomicInteger getCounter(Serializable user) {
+        return users.computeIfAbsent(user, k -> new AtomicInteger());
+    }
+
+    public Set<Serializable> getActiveUsers() {
+        return users.entrySet().stream()
+                .filter(entry -> entry.getValue().intValue() > 0)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toSet());
+    }
 
     public void submit() {
         System.out.println("websocket triggered");
-        test.send("Hello World!");
+
+        Calendar now = Calendar.getInstance();
+
+        String time = now.get(Calendar.HOUR_OF_DAY) + ":" +
+                now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND);
+
+        test.send(time);
+        chat.send("Hallooooooooooo");
+        test.send("Hallooooooooooo123");
     }
 
     public void submitChatMsg() {
