37bc388d008c38d224df5c76f36447187a3aaf54
Philip Riecks <philip@riecks.io>
2018-07-27 09:39:43 +0200

    add correct encoding for message bundles

---
+++ jsfdefinitiveguide/src/main/java/de/rieckpil/learning/ActiveLocale.java
@@ -0,0 +1,53 @@
+package de.rieckpil.learning;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@Named
+@SessionScoped
+public class ActiveLocale implements Serializable {
+
+    private Locale current;
+    private List<Locale> available;
+
+    @Inject
+    private FacesContext context;
+
+    @PostConstruct
+    public void init() {
+        Application app = context.getApplication();
+        current = app.getViewHandler().calculateLocale(context);
+        available = new ArrayList<>();
+        available.add(app.getDefaultLocale());
+        app.getSupportedLocales().forEachRemaining(available::add);
+    }
+
+    public void reload() {
+        context.getPartialViewContext().getEvalScripts().add("location.replace(location)");
+    }
+
+    public Locale getCurrent() {
+        return current;
+    }
+
+    public String getLanguageTag() {
+        return current.toLanguageTag();
+    }
+
+    public void setLanguageTag(String languageTag) {
+        current = Locale.forLanguageTag(languageTag);
+    }
+
+    public List<Locale> getAvailable() {
+        return available;
+    }
+
+}
