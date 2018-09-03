528547aaf9a5740a8163c9b691a7e0f8e51a94f3
Philip Riecks <philip@riecks.io>
2018-09-03 21:00:07 +0200

    add asynchronous EJB method with result type

--- learning-java-ee-8/src/main/java/de/rieckpil/learning/control/CarProcessor.java
+++ learning-java-ee-8/src/main/java/de/rieckpil/learning/control/CarProcessor.java
@@ -2,15 +2,21 @@ package de.rieckpil.learning.control;
 
 import de.rieckpil.learning.entity.Car;
 
+import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.LockSupport;
 
 @Stateless
+@Asynchronous
 public class CarProcessor {
 
-    @Asynchronous
     public void processNewCar(Car car) {
         LockSupport.parkNanos(2_000_000_000L);
+    }
+
+    public Future<String> calculateTax(Car car) {
+        return new AsyncResult<String>("too expensive");
     }
 }
