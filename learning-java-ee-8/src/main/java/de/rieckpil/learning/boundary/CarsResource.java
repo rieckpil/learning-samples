9febd4bb252013732b63f54f6d27be989b4c7a5b
Philip Riecks <philip@riecks.io>
2018-09-04 20:52:45 +0200

    Add Asnyc JAX-RS endpoint example

--- learning-java-ee-8/src/main/java/de/rieckpil/learning/boundary/CarsResource.java
+++ learning-java-ee-8/src/main/java/de/rieckpil/learning/boundary/CarsResource.java
@@ -3,6 +3,8 @@ package de.rieckpil.learning.boundary;
 import de.rieckpil.learning.entity.Car;
 import de.rieckpil.learning.entity.Specification;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -10,7 +12,11 @@ import javax.json.stream.JsonCollectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.List;
 
@@ -24,6 +30,9 @@ public class CarsResource {
 
     @Context
     UriInfo uriInfo;
+
+    @Resource
+    ManagedExecutorService managedExecutorService;
 
     @GET
     public JsonArray retrieveCars() {
@@ -47,7 +56,8 @@ public class CarsResource {
     }
 
     @POST
-    public Response createCar(@Valid @NotNull Specification specification) {
+    public void createCarAsync(@Valid @NotNull Specification specification,
+                               @Suspended AsyncResponse asyncResponse) {
 
         // Color color = Color.valueOf(jsonObject.getString("color"));
         // EngineType engineType = EngineType.valueOf(jsonObject.getString("engineType"));
@@ -59,9 +69,10 @@ public class CarsResource {
                 .path(CarsResource.class, "retrieveCar")
                 .build(car.getIdentifier());
 
-        return Response.status(Response.Status.CREATED)
-                .header(HttpHeaders.LOCATION, uri)
-                .build();
+        // return Response.status(Response.Status.CREATED)
+        //       .header(HttpHeaders.LOCATION, uri)
+        //     .build();
+        managedExecutorService.execute(() -> asyncResponse.resume(uri));
     }
 
 }
