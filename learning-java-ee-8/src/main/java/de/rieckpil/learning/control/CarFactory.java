de15b3908bdce282f7ebdefeb4262530d543904c
Philip Riecks <philip@riecks.io>
2018-09-04 20:48:49 +0200

    add example for ManagedScheduledExecutorService

--- learning-java-ee-8/src/main/java/de/rieckpil/learning/control/CarFactory.java
+++ learning-java-ee-8/src/main/java/de/rieckpil/learning/control/CarFactory.java
@@ -4,12 +4,19 @@ import de.rieckpil.learning.entity.Car;
 import de.rieckpil.learning.entity.Color;
 import de.rieckpil.learning.entity.Specification;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 // @Dependent is added at default
 public class CarFactory {
+
+    @Resource
+    ManagedScheduledExecutorService managedScheduledExecutorService;
 
     @Inject
     @Diesel
@@ -26,5 +33,14 @@ public class CarFactory {
         car.setColor(specification.getColor() == null ? defaultColor : specification.getColor());
         car.setEngineType(specification.getEngineType());
         return car;
+    }
+
+    @PostConstruct
+    public void activateTimer() {
+        managedScheduledExecutorService.scheduleAtFixedRate(this::doSomething, 60, 10, TimeUnit.SECONDS);
+    }
+
+    public void doSomething() {
+        System.out.println("DO BAR");
     }
 }
