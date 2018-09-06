5ae7c6ba67a39c13523ea794390a5ef2158785b5
Philip Riecks <philip@riecks.io>
2018-09-06 16:28:55 +0200

    add new project for evaluating transactions with spring

---
+++ spring-transaction-management/src/main/java/de/rieckpil/learning/springtransactionmanagement/SpringTransactionManagementApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springtransactionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionManagementApplication.class, args);
+    }
+}
---
+++ spring-transaction-management/src/test/java/de/rieckpil/learning/springtransactionmanagement/SpringTransactionManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.springtransactionmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTransactionManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
