0b5559c017ee4bb3626c1e4ce35eb806ae25d1da
Philip Riecks <philip@riecks.io>
2018-09-08 11:45:44 +0200

    add custom exception handler for invalid data input

---
+++ spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package de.rieckpil.learning;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return super.handleExceptionInternal(ex, "There was a problem", headers, status, request);
+	}
+
+}
