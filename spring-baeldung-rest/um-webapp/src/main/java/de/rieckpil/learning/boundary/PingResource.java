11147fa47465322a15cae1e9ecbfe2eee9ec4b33
Philip Riecks <philip@riecks.io>
2018-09-10 19:18:08 +0200

    add Spring Security to the project

--- spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/boundary/PingResource.java
+++ spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/boundary/PingResource.java
@@ -1,5 +1,6 @@
 package de.rieckpil.learning.boundary;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingResource {
 	
 	@GetMapping
+	@Secured("ROLE")
 	public String sayHello() {
 		return "Hello World";
 	}
