d93ad5ce6a3e32fc9a15e49dc5352c207326bc57
Philip Riecks <mail@philipriecks.de>
2018-09-14 09:55:36 +0200

    add swagger to the project

--- spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/ResourceServerConfiguration.java
+++ spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/ResourceServerConfiguration.java
@@ -3,6 +3,7 @@ package de.rieckpil.learning;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -39,6 +40,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 		// @formatter:off
 		http
 			.authorizeRequests()
+				.antMatchers("/swagger-ui.html").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/**").permitAll()
 				.anyRequest().authenticated()
 			.and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
