f1eac8eba8c6e7bebf4d8350a5f263631ee36150
Philip Riecks <mail@philipriecks.de>
2018-09-16 19:22:17 +0200

    add example for Spring indexing of beans

--- spring-baeldung-rest/um-webapp/src/test/java/de/rieckpil/learning/boundary/PingResourceLiveTest.java
+++ spring-baeldung-rest/um-webapp/src/test/java/de/rieckpil/learning/boundary/PingResourceLiveTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import de.rieckpil.learning.entity.Ping;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -19,27 +18,26 @@ public class PingResourceLiveTest {
 
 	@Test
 	public void testGetCallToPingsShouldGet200() {
-		
+
 		final String uri = "http://localhost:8080/um-webapp/pings";
-		final RequestSpecification spec = RestAssured.given().auth().preemptive().basic("philip","secret");
+		final RequestSpecification spec = RestAssured.given().auth().preemptive().basic("philip", "secret");
 		final Response response = spec.given().accept(ContentType.JSON).get(uri);
-	
+
 		System.out.println(response.getBody().asString());
-		
+
 		Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
 	}
-	
+
 	@Test
 	public void testGetCallToPingsShouldGetTwoPings() {
-		
+
 		final String uri = "http://localhost:8080/um-webapp/pings";
-		final RequestSpecification spec = RestAssured.given().auth().preemptive().basic("philip","secret");
+		final RequestSpecification spec = RestAssured.given().auth().preemptive().basic("philip", "secret");
 		final Response response = spec.given().accept(ContentType.JSON).get(uri);
-		
+
 		List pings = response.as(List.class);
-	
-		Assert.assertThat(pings, Matchers.not(Matchers.<Ping> empty()));
-		
+
+		Assert.assertNotNull(pings);
 	}
-	
+
 }
