9d817fcdf63a95b952cfda269b9483bcbf2363cd
Philip Riecks <mail@philipriecks.de>
2018-09-16 17:32:51 +0200

    add shallow Etag with Spring Boot example

--- spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/UmWebappApplication.java
+++ spring-baeldung-rest/um-webapp/src/main/java/de/rieckpil/learning/UmWebappApplication.java
@@ -1,12 +1,31 @@
 package de.rieckpil.learning;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class UmWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmWebappApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean<Filter> someFilterRegistration() {
+		final FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(etagFilter());
+		registration.setName("etagFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+	
+	@Bean(name = "etagFilter")
+	public Filter etagFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
