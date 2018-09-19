bd069de16d16a6dc883923d0f1525b2e4b23ec5b
Philip Riecks <mail@philipriecks.de>
2018-09-19 08:34:55 +0200

    enable more detailed hibernate statistics

--- master-hibernate-jpa-spring-boot/src/main/resources/data.sql
+++ master-hibernate-jpa-spring-boot/src/main/resources/data.sql
@@ -12,3 +12,6 @@ INSERT INTO person (id, name, location, birth_date) VALUES (1002, 'Duke', 'HZA',
 INSERT INTO person (id, name, location, birth_date) VALUES (1003, 'John', 'HZA', '2018-09-17 20:55:42.697');
 INSERT INTO person (id, name, location, birth_date) VALUES (1004, 'Tom', 'HZA', '2018-09-17 20:55:42.697');
 INSERT INTO person (id, name, location, birth_date) VALUES (1005, 'Tim', 'HZA', '2018-09-17 20:55:42.697');
+
+INSERT INTO course (id, name) VALUES (1000, 'in28Minutes');
+INSERT INTO course (id, name) VALUES (1001, 'SpringframeworkGuru');
---
+++ master-hibernate-jpa-spring-boot/src/test/java/de/rieckpil/learning/repositories/CourseRepositoryTest.java
@@ -0,0 +1,38 @@
+package de.rieckpil.learning.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.rieckpil.learning.MasterHibernateJpaSpringBootApplication;
+import de.rieckpil.learning.entity.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MasterHibernateJpaSpringBootApplication.class)
+public class CourseRepositoryTest {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Test
+	public void testFindById() {
+		Course course = courseRepository.findById(1000L);
+
+		assertNotNull(course);
+		assertEquals("in28Minutes", course.getName());
+	}
+	
+	@Test
+	public void testDeleteById() {
+		courseRepository.deleteById(1001L);
+		Course course = courseRepository.findById(1001L);
+		assertNull(course);
+	}
+
+}
