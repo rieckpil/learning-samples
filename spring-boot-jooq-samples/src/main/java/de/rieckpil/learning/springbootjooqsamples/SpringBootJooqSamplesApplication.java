30a5e3d6a286967326ae4207a845dec7346cecfc
Philip Riecks <philip@riecks.io>
2018-09-23 22:30:40 +0200

    init new project

---
+++ spring-boot-jooq-samples/src/main/java/de/rieckpil/learning/springbootjooqsamples/SpringBootJooqSamplesApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.springbootjooqsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJooqSamplesApplication.class, args);
+    }
+}
