96a57cf87aabd62a9e5ed1f3d2901b16907f8f03
Philip Riecks <mail@philipriecks.de>
2018-10-07 09:54:35 +0200

    add JUnit test rule for initializing the TestEntityManager

---
+++ javaee7-testing/sample-service/src/test/java/de/rieckpil/learning/order/entity/EntityManagerProvider.java
@@ -0,0 +1,48 @@
+package de.rieckpil.learning.order.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class EntityManagerProvider implements TestRule {
+
+	private EntityManager em;
+	private EntityTransaction tx;
+
+	private EntityManagerProvider(String unitName) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(unitName);
+		this.em = emf.createEntityManager();
+		this.tx = em.getTransaction();
+	}
+
+	public static EntityManagerProvider withUnit(String unitName) {
+		return new EntityManagerProvider(unitName);
+	}
+
+	public EntityTransaction tx() {
+		return this.tx;
+	}
+
+	public EntityManager em() {
+		return this.em;
+	}
+
+	@Override
+	public Statement apply(Statement base, Description description) {
+		return new Statement() {
+
+			@Override
+			public void evaluate() throws Throwable {
+				base.evaluate();
+				em.clear();
+			}
+
+		};
+	}
+
+}
--- javaee7-testing/sample-service/src/test/java/de/rieckpil/learning/order/entity/OrderIT.java
+++ javaee7-testing/sample-service/src/test/java/de/rieckpil/learning/order/entity/OrderIT.java
@@ -1,30 +1,18 @@
 package de.rieckpil.learning.order.entity;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class OrderIT {
 
-	private EntityManager em;
-	private EntityTransaction tx;
+	@Rule
+	public EntityManagerProvider provider = EntityManagerProvider.withUnit("it");
 
-	@Before
-	public void init() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("it");
-		this.em = emf.createEntityManager();
-		this.tx = em.getTransaction();
-	}
-	
 	@Test
 	public void crud() {
-		this.tx.begin();
-		this.em.merge(new Order("42"));
+		this.provider.tx().begin();
+		this.provider.em().merge(new Order("42"));
 		System.out.println("sucessfully stored the order 42");
-		this.tx.commit();
+		this.provider.tx().commit();
 	}
 }
