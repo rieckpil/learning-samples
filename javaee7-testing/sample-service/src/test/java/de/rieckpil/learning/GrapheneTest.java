b675e9ed562632308756c7c103f3053d6169c6a1
Philip Riecks <mail@philipriecks.de>
2018-10-10 14:46:58 +0200

    add simple test with Graphene

---
+++ javaee7-testing/sample-service/src/test/java/de/rieckpil/learning/GrapheneTest.java
@@ -0,0 +1,21 @@
+package de.rieckpil.learning;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(Arquillian.class)
+public class GrapheneTest {
+
+	@Drone
+	WebDriver browser;
+
+	@Test
+	public void validateAirhacksPage() {
+		browser.get("http://airhacks.com");
+		System.out.println(browser.getTitle());
+	}
+
+}
