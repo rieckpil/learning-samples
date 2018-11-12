53f569a545206de4883f09d7f9e782993f000b86
Philip Riecks <philip@riecks.io>
2018-11-12 17:31:32 +0100

    add example for new HttpClient in JDK 11

--- spring-boot-java-11/src/main/java/de/rieckpil/learning/springbootjava10/NewCapabilities.java
+++ spring-boot-java-11/src/main/java/de/rieckpil/learning/springbootjava10/NewCapabilities.java
@@ -3,6 +3,11 @@ package de.rieckpil.learning.springbootjava10;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -20,21 +25,33 @@ public class NewCapabilities implements CommandLineRunner {
 
 		List<Integer> list = List.of(1, 334, 12, 121, 99, 1, 802);
 
-		list
-			.stream()
-			.takeWhile(e -> e < 99)
-			.forEach(System.out::println);
+		list.stream().takeWhile(e -> e < 99).forEach(System.out::println);
 
-		IntStream
-			.iterate(1, n -> n + 1)
-			.takeWhile(n -> n < 10)
-			.forEach(System.out::println);
+		IntStream.iterate(1, n -> n + 1).takeWhile(n -> n < 10).forEach(System.out::println);
 
 		System.out.println(System.getProperty("java.io.tmpdir"));
 
 		File file = File.createTempFile("java11", ".txt");
 		writeToFile(new FileWriter(file));
 
+		HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2)
+				.connectTimeout(Duration.ofSeconds(2)).build();
+
+		String astroUrl = "http://api.open-notify.org/astros.json";
+		var request = HttpRequest.newBuilder().uri(URI.create(astroUrl)).GET().build();
+
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+		String output = response.body();
+
+		System.out.println(output);
+
+		System.out.println("Sending async");
+
+		client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body)
+				.thenAccept(System.out::println);
+
+		System.out.println("Async requested ...");
 	}
 
 	private void writeToFile(FileWriter writer) {
