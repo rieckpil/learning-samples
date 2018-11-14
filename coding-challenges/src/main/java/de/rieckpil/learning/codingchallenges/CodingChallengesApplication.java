61ee1764479d6515d94a1aaaa97fd4a482497595
Philip Riecks <philip@riecks.io>
2018-11-14 12:06:55 +0100

    add new project for coding challenges

---
+++ coding-challenges/src/main/java/de/rieckpil/learning/codingchallenges/CodingChallengesApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.learning.codingchallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengesApplication.class, args);
+	}
+}
---
+++ coding-challenges/src/test/java/de/rieckpil/learning/codingchallenges/CodingChallengesApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rieckpil.learning.codingchallenges;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodingChallengesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
