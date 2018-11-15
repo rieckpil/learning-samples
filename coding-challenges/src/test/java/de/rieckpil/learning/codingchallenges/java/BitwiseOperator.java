69014796f8d39a4b89d2ab6f01c177d00e59f77b
Philip Riecks <philip@riecks.io>
2018-11-15 21:14:38 +0100

    add further challenge examples

---
+++ coding-challenges/src/test/java/de/rieckpil/learning/codingchallenges/java/BitwiseOperator.java
@@ -0,0 +1,21 @@
+package de.rieckpil.learning.codingchallenges.java;
+
+import org.junit.Test;
+
+public class BitwiseOperator {
+
+	@Test
+	public void test() {
+
+		int a = 60;
+		int b = 13;
+
+		System.out.println(1 << 4);
+		System.out.println("XOR: " + (a ^ b));
+		System.out.println("AND: " + (a & b));
+		System.out.println("OR: " + (a | b));
+		System.out.println(Long.valueOf("1").hashCode());
+		System.out.println(Long.valueOf("1").hashCode());
+
+	}
+}
---
+++ coding-challenges/src/test/java/de/rieckpil/learning/codingchallenges/java/HashMapThreadSafetyTest.java
@@ -0,0 +1,81 @@
+package de.rieckpil.learning.codingchallenges.java;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.Test;
+
+public class HashMapThreadSafetyTest {
+
+	private static final int UUIDS_PER_THREAD = 5000;
+	private static final int AMOUNT_OF_THREADS = 15;
+
+	@Test
+	public void proveHashMapIsNotThreadSafeFirst() throws InterruptedException {
+
+		Map<String, String> cut = new HashMap<>();
+
+		List<Thread> threads = new ArrayList<>();
+
+		for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+			Runnable runnable = () -> {
+				for (int j = 0; j < UUIDS_PER_THREAD; j++) {
+					cut.put(UUID.randomUUID().toString(), "John Doe");
+				}
+
+			};
+
+			Thread t = new Thread(runnable);
+			t.start();
+			threads.add(t);
+		}
+
+		for (Thread thread : threads) {
+			thread.join();
+		}
+
+		assertEquals(UUIDS_PER_THREAD * AMOUNT_OF_THREADS, cut.size());
+
+	}
+
+	/**
+	 * This test addresses the weakness of the resizing during a .put(K,V) operation
+	 * of the HashTable. If the size after the insertion of a new map entry is
+	 * greater then the threshold (capacity * load factor), the HashTable will
+	 * resize to a doubled size. During this .resize() operation (rehashing and
+	 * rebuilding the internal data structure of the HashMap) a concurrent thread
+	 * wants to access an already inserted value but will get a null reference in
+	 * the worst case.
+	 */
+	@Test(timeout = 10_000)
+	public void proveHashMapIsNotThreadSafeSecond() {
+
+		Map<String, String> cut = new HashMap<>();
+
+		String initialKey = UUID.randomUUID().toString();
+
+		cut.put(initialKey, "John Doe");
+
+		Runnable runnable = () -> {
+			for (int i = 0; i < 1_000_000; i++) {
+				cut.put(UUID.randomUUID().toString(), "John Doe " + i);
+			}
+			System.out.println("Finished inserting 1_000_000 UUIDs");
+		};
+
+		Thread t = new Thread(runnable);
+		t.start();
+
+		while (true) {
+			if (cut.get(initialKey) == null) {
+				System.out.println("Can't find initial UUID.");
+			}
+		}
+	}
+
+}
