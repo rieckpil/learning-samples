e02021196127dce69fc79d94dc5867cc9cdd0360
Philip Riecks <mail@philipriecks.de>
2018-11-21 18:12:52 +0100

    add generics intro challenge

---
+++ coding-challenges/src/main/java/de/rieckpil/learning/codingchallenges/java/GenericsIntro.java
@@ -0,0 +1,33 @@
+package de.rieckpil.learning.codingchallenges.java;
+
+import java.lang.reflect.Method;
+
+public class GenericsIntro {
+	public static void main(String args[]) {
+		Printer myPrinter = new Printer();
+		Integer[] intArray = { 1, 2, 3 };
+		String[] stringArray = { "Hello", "World" };
+		myPrinter.printArray(intArray);
+		myPrinter.printArray(stringArray);
+		int count = 0;
+
+		for (Method method : Printer.class.getDeclaredMethods()) {
+			String name = method.getName();
+
+			if (name.equals("printArray"))
+				count++;
+		}
+
+		if (count > 1)
+			System.out.println("Method overloading is not allowed!");
+	}
+}
+
+class Printer {
+	public <T> void printArray(T[] array) {
+		for (T value : array) {
+			System.out.println(value);
+		}
+	}
+
+}
