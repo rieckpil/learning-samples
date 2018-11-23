aba1b1624e32dd1680fe89329a15db7e459055b0
Philip Riecks <mail@philipriecks.de>
2018-11-23 17:41:30 +0100

    add intro for abstract class

---
+++ coding-challenges/src/main/java/de/rieckpil/learning/codingchallenges/java/AbstractClassIntro.java
@@ -0,0 +1,37 @@
+package de.rieckpil.learning.codingchallenges.java;
+
+import java.util.Scanner;
+
+abstract class Book {
+	String title;
+
+	abstract void setTitle(String s);
+
+	String getTitle() {
+		return title;
+	}
+
+}
+
+class MyBook extends Book {
+
+	@Override
+	void setTitle(String s) {
+		this.title = s;
+	}
+}
+
+public class AbstractClassIntro {
+
+	public static void main(String[] args) {
+		// Book new_novel=new Book(); This line prHMain.java:25: error: Book is
+		// abstract; cannot be instantiated
+		Scanner sc = new Scanner(System.in);
+		String title = sc.nextLine();
+		MyBook new_novel = new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: " + new_novel.getTitle());
+		sc.close();
+
+	}
+}
