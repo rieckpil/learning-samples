a0dba1ab2dbf64cae9e6beca0185c66cd6e2078a
Philip Riecks <mail@philipriecks.de>
2019-01-09 19:58:17 +0100

    add CSV handling with Apache Commons CSV sample

---
+++ csv-samples/src/main/java/de/rieckpil/learning/commons/CSVWriter.java
@@ -0,0 +1,29 @@
+package de.rieckpil.learning.commons;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+public class CSVWriter {
+	private static final String SAMPLE_CSV_FILE = "sample-apache-commons.csv";
+
+	public static void main(String[] args) throws IOException {
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE));
+				CSVPrinter csvPrinter = new CSVPrinter(writer,
+						CSVFormat.DEFAULT.withHeader("ID", "Name", "Designation", "Company").withDelimiter(','));) {
+
+			csvPrinter.printRecord("1", "Sundar Pichai", "CEO", "Google");
+			csvPrinter.printRecord("2", "Satya Nadella", "CEO", "Microsoft");
+			csvPrinter.printRecord("3", "Tim cook", "CEO", "Apple");
+			csvPrinter.printRecord(Arrays.asList("4", "Mark Zuckerberg", "CEO", "Facebook"));
+			csvPrinter.flush();
+			System.out.println("Finished writing CSV");
+		}
+	}
+
+}
