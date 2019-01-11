9a9af2da659b7f4dde3fca2995604dc72e094bfc
Philip Riecks <mail@philipriecks.de>
2019-01-11 18:16:46 +0100

    inject sql DataSource to display JDBC version

--- spring-boot-postgres-stored-procedure/src/main/java/de/rieckpil/learning/DatabaseFiller.java
+++ spring-boot-postgres-stored-procedure/src/main/java/de/rieckpil/learning/DatabaseFiller.java
@@ -1,9 +1,11 @@
 package de.rieckpil.learning;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -16,11 +18,18 @@ public class DatabaseFiller {
 	@Autowired
 	private UserRepository repository;
 
+	@Autowired
+	private DataSource dataSource;
+
 	@PersistenceContext
 	private EntityManager em;
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void init() {
+	public void init() throws SQLException {
+
+		System.out.println("Auto commit: " + dataSource.getConnection().getAutoCommit());
+		System.out.println("JDBC Major version: " + dataSource.getConnection().getMetaData().getJDBCMajorVersion());
+		System.out.println("JDBC Minor version: " + dataSource.getConnection().getMetaData().getJDBCMinorVersion());
 
 		for (int i = 0; i < 100; i++) {
 			repository.save(new User("Duke", UUID.randomUUID().toString()));
