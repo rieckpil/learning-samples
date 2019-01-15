feef61fec6de58e40fd4a2d193da730697850789
Philip Riecks <mail@philipriecks.de>
2019-01-15 20:06:04 +0100

    add logging example for Hibernate

--- hibernate-course-vlad/src/main/java/de/rieckpil/learning/HibernateCourseVladApplication.java
+++ hibernate-course-vlad/src/main/java/de/rieckpil/learning/HibernateCourseVladApplication.java
@@ -1,14 +1,29 @@
 package de.rieckpil.learning;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.rieckpil.learning.domain.Post;
+
 @SpringBootApplication
-public class HibernateCourseVladApplication {
+public class HibernateCourseVladApplication implements CommandLineRunner {
+
+	@Autowired
+	private EntityManager em;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateCourseVladApplication.class, args);
 	}
 
-}
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+		this.em.persist(new Post("Hello World!"));
+	}
 
+}
