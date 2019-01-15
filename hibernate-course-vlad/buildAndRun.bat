6756db27ddd7b22e4395717c352e7215877faf31
Philip Riecks <mail@philipriecks.de>
2019-01-15 19:41:22 +0100

    replace Hibernate course repository

--- hibernate-course-vlad/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t de.rieckpil.learning/hibernate-course-vlad .
-call docker rm -f hibernate-course-vlad
-call docker run -d -p 8080:8080 -p 4848:4848 --name hibernate-course-vlad de.rieckpil.learning/hibernate-course-vlad
--- hibernate-course-vlad/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t de.rieckpil.learning/hibernate-course-vlad .
-docker rm -f hibernate-course-vlad || true && docker run -d -p 8080:8080 -p 4848:4848 --name hibernate-course-vlad de.rieckpil.learning/hibernate-course-vlad
