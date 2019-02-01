1f1f46295059779c7a379fc6f8a4be88b5ce1479
Philip Riecks <rieckpil@schaeffler.com>
2019-02-01 15:00:30 +0100

    add new project for websocket test case

---
+++ spring-websocket-full-feature-broker/src/main/java/de/rieckpil/learning/SpringWebsocketFullFeatureBrokerApplication.java
@@ -0,0 +1,14 @@
+package de.rieckpil.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketFullFeatureBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketFullFeatureBrokerApplication.class, args);
+	}
+
+}
+
---
+++ spring-websocket-full-feature-broker/src/test/java/de/rieckpil/learning/SpringWebsocketFullFeatureBrokerApplicationTests.java
@@ -0,0 +1,17 @@
+package de.rieckpil.learning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebsocketFullFeatureBrokerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
