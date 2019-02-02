9d3ab4b10ac13599d2dec6d75e041632daeb5f84
Philip Riecks <mail@philipriecks.de>
2019-02-02 15:47:49 +0100

    update websockt config

--- spring-websocket-full-feature-broker/src/main/java/de/rieckpil/learning/WebSocketConfig.java
+++ spring-websocket-full-feature-broker/src/main/java/de/rieckpil/learning/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableStompBrokerRelay("/queue/", "/topic/");
+		config.enableStompBrokerRelay("/topic/");
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
