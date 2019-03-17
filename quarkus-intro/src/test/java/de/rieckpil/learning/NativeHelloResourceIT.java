006c33379fc9700a8a5b626c381915134490da29
Philip Riecks <mail@philipriecks.de>
2019-03-17 18:46:18 +0100

    add first quarkus example

---
+++ quarkus-intro/src/test/java/de/rieckpil/learning/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package de.rieckpil.learning;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ wordpress-backup/README.md
@@ -0,0 +1,3 @@
+```
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=wordpress -e MYSQL_USER=wordpress -e MYSQL_DATABASE=wordpress -p 3306:3306 mysql:5.7 
+```
