185ba7c63a917c3450598aff1063d1cb90b869a5
Philip Riecks <mail@philipriecks.de>
2020-01-20 17:40:37 +0100

    update WildFly example for Hypersistence

--- wildfly-hyperistence/buildAndRun.sh
+++ wildfly-hyperistence/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t de.rieckpil.blog/wildfly-hyperistence .
-docker rm -f wildfly-hyperistence || true && docker run -d -p 8080:8080 -p 9990:9990 --name wildfly-hyperistence de.rieckpil.blog/wildfly-hyperistence
+docker rm -f wildfly-hyperistence || true && docker run -p 8080:8080 -p 9990:9990 -it de.rieckpil.blog/wildfly-hyperistence /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
--- wildfly-hyperistence/src/main/java/de/rieckpil/blog/JpaAnalyzer.java
+++ wildfly-hyperistence/src/main/java/de/rieckpil/blog/JpaAnalyzer.java
@@ -1,8 +1,5 @@
 package de.rieckpil.blog;
 
-import io.hypersistence.optimizer.HypersistenceOptimizer;
-import io.hypersistence.optimizer.core.config.JpaConfig;
-
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -13,12 +10,12 @@ import javax.persistence.PersistenceUnit;
 @Singleton
 public class JpaAnalyzer {
 
-    @PersistenceUnit
-    private EntityManagerFactory entityManagerFactory;
+  @PersistenceUnit
+  private EntityManagerFactory entityManagerFactory;
 
-    @PostConstruct
-    public void init() {
-        System.out.println("Analyzing JPA & Hibernate setup");
-        new HypersistenceOptimizer(new JpaConfig(entityManagerFactory)).init();
-    }
+  @PostConstruct
+  public void init() {
+    System.out.println("Analyzing JPA & Hibernate setup");
+    // new HypersistenceOptimizer(new JpaConfig(entityManagerFactory)).init();
+  }
 }
