1ced68da3710db448fe18122a7a63bf1115efc3e
rieckpil <philip.riecks@scout24.com>
2020-02-23 10:54:32 +0100

    add further example to test when embedded servlet container is started

--- spring-boot-testing-weblayer/src/test/kotlin/de/rieckpil/learning/SpringBootTestingWeblayerApplicationTests.kt
+++ spring-boot-testing-weblayer/src/test/kotlin/de/rieckpil/learning/SpringBootTestingWeblayerApplicationTests.kt
@@ -1,13 +1,38 @@
 package de.rieckpil.learning
 
+import com.fasterxml.jackson.databind.JsonNode
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.client.WebClient
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringBootTestingWeblayerApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+  @LocalServerPort
+  private lateinit var port: Integer
+
+  @Autowired
+  private lateinit var webTestClient: WebTestClient
+
+  @Test
+  fun contextLoads() {
+
+    println(WebClient.builder()
+      .baseUrl("http://localhost:$port")
+      .build()
+      .get()
+      .uri("/api")
+      .accept(MediaType.APPLICATION_JSON)
+      .retrieve()
+      .toEntity(String::class.java)
+      .block())
+
+    println(webTestClient.get().uri("/api").exchange().returnResult(String.javaClass))
+  }
 
 }
