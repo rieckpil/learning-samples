552cbb42fa7d25c243dbb2dabb91605ceb53cedc
rieckpil <philip.riecks@scout24.com>
2020-03-12 19:29:07 +0100

    add further Thymeleaf example

--- spring-boot-stackoverflow/src/main/java/de/rieckpl/learning/SampleViewController.java
+++ spring-boot-stackoverflow/src/main/java/de/rieckpl/learning/SampleViewController.java
@@ -3,7 +3,10 @@ package de.rieckpl.learning;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/public")
@@ -12,6 +15,16 @@ public class SampleViewController {
   @GetMapping("/index")
   public String getIndex(Model model) {
     model.addAttribute("message", "duke");
+    model.addAttribute("order", new Order());
     return "index";
+  }
+
+  @PostMapping("/index")
+  public ModelAndView sendOrder(@ModelAttribute Order order) {
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("index");
+    modelAndView.addObject("message", order.getAmountValue());
+    System.out.println(order);
+    return modelAndView;
   }
 }
