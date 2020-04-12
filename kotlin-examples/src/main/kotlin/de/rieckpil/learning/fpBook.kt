f16437ea828c142ba310302063d1a23df8a4bd72
rieckpil <philip.riecks@scout24.com>
2020-04-12 10:42:19 +0200

    add next exmaples

--- kotlin-examples/src/main/kotlin/de/rieckpil/learning/fpBook.kt
+++ kotlin-examples/src/main/kotlin/de/rieckpil/learning/fpBook.kt
@@ -2,7 +2,15 @@ package de.rieckpil.learning
 
 fun main() {
   println(factorial(7))
-  println(fib(5))
+  println(fib(10))
+  println(findFirst(arrayOf("Duke", "Foo", "Mike", "Phil", "Tom")) { it == "Phil" })
+  println(findFirst(arrayOf(true, false, true, false)) { !it })
+  println(isSorted(listOf(1, 10, 20, 30)) { x, y ->
+    x < y
+  })
+  println(isSorted(listOf("Mike", "Hansi","Dukeduke", "Alphabetz")) { x, y ->
+    x.length < y.length
+  })
 }
 
 
@@ -18,4 +26,31 @@ fun fib(i: Int): Int {
     if (n <= 1) n
     else go(n - 1) + go(n - 2)
   return go(i)
+}
+
+fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int {
+  tailrec fun loop(n: Int): Int =
+    when {
+      n >= xs.size -> -1
+      p(xs[n]) -> n
+      else -> loop(n + 1)
+    }
+  return loop(0)
+}
+
+val <T> List<T>.tail: List<T>
+  get() = drop(1)
+
+val <T> List<T>.head: T
+  get() = first()
+
+fun <A> isSorted(aa: List<A>, ordered: (A, A) -> Boolean): Boolean {
+  tailrec fun loop(head: A, tail: List<A>): Boolean =
+    when {
+      head == null -> true
+      tail.isEmpty() -> true
+      ordered(head, tail.head) -> loop(tail.head, tail.tail)
+      else -> false
+    }
+  return loop(aa.head, aa.tail)
 }
