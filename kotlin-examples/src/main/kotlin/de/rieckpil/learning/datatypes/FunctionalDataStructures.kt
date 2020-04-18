859e3454f3ac2bdd37bb849a51f81be5a11e5ed7
rieckpil <philip.riecks@scout24.com>
2020-04-18 17:08:09 +0200

    add next exercises

--- kotlin-examples/src/main/kotlin/de/rieckpil/learning/datatypes/FunctionalDataStructures.kt
+++ kotlin-examples/src/main/kotlin/de/rieckpil/learning/datatypes/FunctionalDataStructures.kt
@@ -9,6 +9,8 @@ sealed class List<out A> {
       return if (aa.isEmpty()) Nil else Cons(aa[0], of(*tail))
     }
 
+    fun <A> empty(): List<A> = Nil
+
     fun sum(ints: List<Int>): Int =
       when (ints) {
         is Nil -> 0
@@ -18,9 +20,7 @@ sealed class List<out A> {
     fun product(doubles: List<Double>): Double =
       when (doubles) {
         is Nil -> 1.0
-        is Cons ->
-          if (doubles.head == 0.0) 0.0
-          else doubles.head * product(doubles.tail)
+        is Cons -> doubles.head * product(doubles.tail)
       }
 
     fun <A> tail(xs: List<A>): List<A> =
@@ -49,6 +49,39 @@ sealed class List<out A> {
         is Cons -> if (f(l.head)) dropWhile(l.tail, f) else l
       }
     }
+
+    fun <A> init(l: List<A>): List<A> =
+      when (l) {
+        is Nil -> throw java.lang.IllegalStateException("Not possible here")
+        is Cons -> when (l.tail) {
+          is Nil -> Nil
+          is Cons -> Cons(l.head, init(l.tail))
+        }
+      }
+
+    fun sumRight(ints: List<Int>): Int = foldRight(ints, 0, { a, b -> a + b })
+
+    fun productRight(dbs: List<Double>): Double = foldRight(dbs, 1.0, { a, b -> a * b })
+
+    fun <A> length(l: List<A>): Int = foldRight(l, 0) { _, acc -> acc + 1 }
+
+    fun <A, B> foldRight(l: List<A>, z: B, f: (A, B) -> B): B =
+      when (l) {
+        is Nil -> z
+        is Cons -> f(l.head, foldRight(l.tail, z, f))
+      }
+
+    tailrec fun <A, B> foldLeft(l: List<A>, z: B, f: (B, A) -> B): B =
+      when (l) {
+        is Nil -> z
+        is Cons -> foldLeft(l.tail, f(z, l.head), f)
+      }
+
+    fun sumLeft(ints: List<Int>): Int = foldLeft(ints, 0, { a, b -> a + b })
+
+    fun productLeft(ints: List<Double>): Double = foldLeft(ints, 1.0, { a, b -> a * b })
+
+    fun <A> lengthLeft(l: List<A>): Int = foldLeft(l, 0, { acc, _ -> acc + 1 })
   }
 }
 
@@ -70,4 +103,15 @@ fun main() {
   }
 
   println(droppedWhile)
+
+  // println(List.init(List.of(1, 2, 3, 4, 5, 6, 7)))
+
+  // val f = { x: Int, y: List<Int> -> Cons(x, y) }
+  // println(List.foldRight(List.of(1, 2, 3), List.empty(), f))
+  // println(List.length(List.of(1, 2, 3, 4, 5)))
+
+
+  println(List.sumLeft(List.of(1, 2, 3, 4)))
+  println(List.lengthLeft(List.of(1, 2, 3, 4)))
+
 }
