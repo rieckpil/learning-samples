65ba6fdc65a3881b01c04cab5b0ab3b3baae4a8f
rieckpil <philip.riecks@scout24.com>
2020-06-25 09:47:32 +0200

    add first working example

---
+++ angular-js-course/order-application/app/app.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import './style.css';
+
+angular
+  .module('order-app', []);
--- angular-js-course/order-application/webpack.config.js
+++ angular-js-course/order-application/webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         }]
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.html/,
         use: 'html-loader'
       }
