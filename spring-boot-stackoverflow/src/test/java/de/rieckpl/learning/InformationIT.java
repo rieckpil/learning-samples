d7cf15db4d20692004100e46cce6f06fb88fe84f
rieckpil <mail@philipriecks.de>
2020-06-25 08:31:18 +0200

    add next examples

---
+++ spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/InformationIT.java
@@ -0,0 +1,25 @@
+package de.rieckpl.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class InformationIT {
+
+  @Autowired
+  private Environment environment;
+
+  @Test
+  public void testEnv() {
+    System.out.println(Arrays.toString(environment.getActiveProfiles()));
+    System.out.println(environment.getProperty("secret.value"));
+    System.out.println(environment.getProperty("secret.value.two"));
+    System.out.println(environment.getProperty("smoke.tests.enabled"));
+  }
+}
