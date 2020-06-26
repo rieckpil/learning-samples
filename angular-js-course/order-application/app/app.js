c38bcf2ec4ab787b0fe6daae32cf68ef184dabc6
rieckpil <philip.riecks@scout24.com>
2020-06-26 14:16:02 +0200

    first controller

--- angular-js-course/order-application/app/app.js
+++ angular-js-course/order-application/app/app.js
@@ -1,5 +1,20 @@
 import angular from 'angular';
 import './style.css';
 
-angular
-  .module('order-app', []);
+const app = angular
+  .module('orderApp', [])
+  .controller('customersController', function init($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+    $scope.customers = [
+      {joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956},
+      {joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99},
+      {joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99},
+      {joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50}
+    ];
+    $scope.doSort = function (propName) {
+      console.log('Fooo');
+      $scope.sortBy = propName;
+      $scope.reverse = !$scope.reverse;
+    };
+  });
