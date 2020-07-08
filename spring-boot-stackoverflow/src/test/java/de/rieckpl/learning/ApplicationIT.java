c8fdcf5c5b4627e1d61ea492d4592dbda575bf4d
rieckpil <mail@philipriecks.de>
2020-07-08 08:33:35 +0200

    example for capturing test results of console

--- spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/ApplicationIT.java
+++ spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/ApplicationIT.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ContextConfiguration(initializers = MongoDbDataInitializer.class)
-class SpringBootStackoverflowApplicationTests {
+class ApplicationIT {
 
   @Autowired
   private Environment environment;
--- spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/NestedTest.java
+++ spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/NestedTest.java
@@ -2,6 +2,7 @@ package de.rieckpl.learning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
