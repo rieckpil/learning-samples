aa47d9177bab82017927a541cb01a6fc8825603e
rieckpil <mail@philipriecks.de>
2020-07-09 07:34:00 +0200

    first steps selenide

---
+++ spring-boot-stackoverflow/src/test/java/de/rieckpl/learning/BasicWT.java
@@ -0,0 +1,8 @@
+package de.rieckpl.learning;
+
+import com.codeborne.selenide.junit5.TextReportExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(TextReportExtension.class)
+public class BasicWT {
+}
