ebc76632d151884bfda26b057b6fb82b501d917e
rieckpil <philip.riecks@scout24.com>
2020-07-24 09:55:25 +0200

    next steps

--- kotlin-examples/src/main/kotlin/de/rieckpil/learning/fpbook/propertytesting/Library.kt
+++ kotlin-examples/src/main/kotlin/de/rieckpil/learning/fpbook/propertytesting/Library.kt
@@ -1,6 +1,7 @@
 package de.rieckpil.learning.fpbook.propertytesting
 
-import de.rieckpil.learning.fpbook.either.Either
+import arrow.core.getOrElse
+import arrow.core.toOption
 import kotlin.math.absoluteValue
 
 data class Gen<A>(val sample: State<RNG, A>) {
@@ -35,14 +36,50 @@ data class Gen<A>(val sample: State<RNG, A>) {
 
 typealias SuccessCount = Int
 typealias FailedCase = String
+typealias TestCases = Int
 
-interface Prop {
-  fun check(): Either<Pair<FailedCase, SuccessCount>, SuccessCount>
-  fun and(p: Prop): Prop = TODO()
+// typealias Result = Either<Pair<FailedCase, SuccessCount>, SuccessCount>
+// typealias Result = Option<Pair<FailedCase, SuccessCount>>
+
+sealed class Result {
+  abstract fun isFalsified(): Boolean
+}
+
+object Passed : Result() {
+  override fun isFalsified(): Boolean = false
+}
+
+data class Falsified(
+  val failure: FailedCase,
+  val successes: SuccessCount
+) : Result() {
+  override fun isFalsified(): Boolean = true
+}
+
+data class Prop(val check: (TestCases, RNG) -> Result) {
+  fun and(p: Prop): Prop = Prop { n: TestCases, rng: RNG ->
+      val resultOne = this.check(n, rng)
+      val resultTwo = p.check(n, rng)
+
+    when {
+      resultOne.isFalsified() -> {
+        resultOne
+      }
+      resultTwo.isFalsified() -> {
+        resultTwo
+      }
+      else -> {
+        Passed
+      }
+    }
+
+  }
+
+  fun or(p: Prop): Prop = TODO()
 }
 
 fun choose(start: Int, stopExclusive: Int): Gen<Int> = Gen(State { rng: RNG -> nonNegativeInt(rng) }
-  .map {it % (stopExclusive - start) + start })
+  .map { it % (stopExclusive - start) + start })
 
 fun <A> unit(a: A): Gen<A> = Gen(State.unit(a))
 
@@ -52,4 +89,32 @@ fun <A> listOfN(n: Int, ga: Gen<A>): Gen<List<A>> = Gen(State.sequence(List(n) {
 
 fun <A> listOf(a: Gen<A>): List<Gen<A>> = TODO()
 
-fun <A> forAll(a: Gen<A>, f: (A) -> Boolean): Prop = TODO()
+fun <A> forAll(ga: Gen<A>, f: (A) -> Boolean): Prop =
+  Prop { n: TestCases, rng: RNG ->
+    randomSequence(ga, rng).mapIndexed { i, a ->
+      try {
+        if (f(a)) Passed
+        else Falsified(a.toString(), i)
+      } catch (e: Exception) {
+        Falsified(buildMessage(a, e), i)
+      }
+    }.take(n)
+      .find { it.isFalsified() }
+      .toOption()
+      .getOrElse { Passed }
+  }
+
+private fun <A> randomSequence(ga: Gen<A>, rng: RNG): Sequence<A> =
+  sequence {
+    val (a: A, rng2: RNG) = ga.sample.run(rng)
+    yield(a)
+    yieldAll(randomSequence(ga, rng2))
+  }
+
+private fun <A> buildMessage(a: A, e: Exception) =
+  """
+  |test case: $a
+  |generated and exception: ${e.message}
+  |stacktrace:
+  |${e.stackTrace.joinToString("\n")}
+""".trimMargin()
