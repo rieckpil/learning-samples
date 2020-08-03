0f7e8f59daf3912539b13991c8e5152436f787a8
rieckpil <mail@philipriecks.de>
2020-08-03 05:54:58 +0200

    add example for nested test class workaround

--- spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/NestedTest.java
+++ spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/NestedTest.java
@@ -2,15 +2,19 @@ package de.rieckpil.learning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class NestedTest {
 
   @Nested
   @DisplayName("Test for calculator")
+  @SpringBootTest
+  @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
   class CalculatorTest {
 
     @Test
@@ -27,6 +31,8 @@ public class NestedTest {
 
   @Nested
   @DisplayName("Test for division")
+  @SpringBootTest
+  @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
   class DivisionTest {
 
     @Test
