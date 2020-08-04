d27224e249af15f168e3feec406072f32ac35c26
rieckpil <mail@philipriecks.de>
2020-08-04 17:31:45 +0200

    next steps

--- hacking-spring-boot-book/src/main/java/de/rieckpil/learning/HomeController.java
+++ hacking-spring-boot-book/src/main/java/de/rieckpil/learning/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
   @GetMapping
   Mono<Rendering> home() {
     return Mono.just(Rendering.view("home.html")
-      .modelAttribute("items", this.itemRepository.findAll())
+      .modelAttribute("items", this.itemRepository.findAll().doOnNext(System.out::println))
       .modelAttribute("cart", this.cartRepository.findById("My Cart").defaultIfEmpty(new Cart("My Cart")))
       .build());
   }
