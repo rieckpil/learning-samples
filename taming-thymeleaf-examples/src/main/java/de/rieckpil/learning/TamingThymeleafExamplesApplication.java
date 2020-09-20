b0c3db1f4c4d565351b6f7969eb735e588f9244e
rieckpil <mail@philipriecks.de>
2020-09-20 17:14:59 +0200

    project to work through Taming Thymeleaf

---
+++ taming-thymeleaf-examples/src/main/java/de/rieckpil/learning/TamingThymeleafExamplesApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamingThymeleafExamplesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TamingThymeleafExamplesApplication.class, args);
+  }
+
+}
