3798e53d1711bdc0e6f899262a873c296d57110f
Philip Riecks <mail@philipriecks.de>
2020-11-10 07:33:28 +0100

    format code

--- spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/SystemEnvMockTest.java
+++ spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/SystemEnvMockTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.when;
-
 public class SystemEnvMockTest {
 
   @Test
   @Disabled
   void mockSystemEnv() {
-    try(MockedStatic<System> mockedSystem = Mockito.mockStatic(System.class)) {
+    try (MockedStatic<System> mockedSystem = Mockito.mockStatic(System.class)) {
       mockedSystem.when(System::getenv).thenReturn("DUKE_42");
       System.out.println(System.getenv());
     }
