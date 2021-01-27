4d574ee630cdc442cfbd154ac5324ee6ba13ee4b
Philip Riecks <mail@philipriecks.de>
2021-01-27 16:17:19 +0100

    add example to inject controller beans

--- spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/NullControllerTest.java
+++ spring-boot-stackoverflow/src/test/java/de/rieckpil/learning/NullControllerTest.java
@@ -1,10 +1,12 @@
 package de.rieckpil.learning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,8 +17,24 @@ class NullControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired(required = false)
+  private ProtobufController protobufController;
+
+  @Autowired(required = false)
+  private NullController nullController;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
   @Test
   void test() throws Exception {
+
+    assertNull(protobufController);
+    assertNotNull(nullController);
+
+    assertNotNull(webApplicationContext.getBean(NullController.class));
+    assertThrows(NoSuchBeanDefinitionException.class, () -> webApplicationContext.getBean(ProtobufController.class));
+
     this.mockMvc.perform(MockMvcRequestBuilders.get("/null"))
       .andExpect(status().isOk());
   }
